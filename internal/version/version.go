25ab9f69487b8883352587f81ee26a96fcf8bac0
Terrence Chen <terrchen@gitsense.com>
2026-02-13 02:35:33 -0500

    Saving

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,12 +1,12 @@
-/*
+/**
  * Component: Version Information
- * Block-UUID: 42ce9290-86bc-45b0-9d43-c50e65ec517e
- * Parent-UUID: N/A
- * Version: 1.0.0
+ * Block-UUID: 0cf4b978-4325-4a48-9c8b-e6a0276ef598
+ * Parent-UUID: 42ce9290-86bc-45b0-9d43-c50e65ec517e
+ * Version: 1.0.1
  * Description: Package version provides version information for the GSC CLI tool.
  * Language: Go
- * Created-at: 2026-02-02T05:46:05.908Z
- * Authors: GLM-4.7 (v1.0.0)
+ * Created-at: 2026-02-13T07:22:40.583Z
+ * Authors: GLM-4.7 (v1.0.0), Gemini 3 Flash (v1.0.1)
  */
 
 
@@ -17,7 +17,7 @@ import "fmt"
 // These variables are set at build time using ldflags
 var (
 	// Version is the current version of the application
-	Version = "1.0.0"
+	Version = "0.1.0"
 	// GitCommit is the git commit hash
 	GitCommit = "unknown"
 	// BuildTime is the timestamp when the binary was built
